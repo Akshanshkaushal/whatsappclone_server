f5cd91aa03fdce149b56a80345e85c36e404b356
Akshanshkaushal <akshanshkaushal9@gmail.com>
2023-12-27 15:15:31 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import Connection from './database/db.js';
+import Routes from './routes/Routes.js';
+
+dotenv.config();
+const app = express();
+
+const PORT = 8000;
+
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+
+Connection(username, password);
+
+app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+
+// Express now has built-in middleware for handling JSON and URL-encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use('/', Routes);
